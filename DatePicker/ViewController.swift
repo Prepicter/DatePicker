079e353064dc26c3d18171f96db038314859d810
dit03 <dit@imac-4.local>
2019-04-17 15:42:18 +0900

    First Commit

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var outLabel: UILabel!
+    @IBOutlet weak var mDataPicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let locale = Locale(identifier: "ko-KO")
+        mDataPicker.locale = locale
+        
+        mDataPicker.datePickerMode = UIDatePicker.Mode.dateAndTime
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyy-MM-dd hh:mm:ss EEE a"
+        let formattedDate = format.string(from: date)
+        outLabel.text = formattedDate
+        
     }
+    
+    
+    
+    @IBAction func valueChanged(_ sender: Any) {
+        let selected = mDataPicker.date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd hh:mm:ss EEE a"
+        outLabel.text = formatter.string(from: selected)
+        
+    }
+    
+    
 
 
 }
